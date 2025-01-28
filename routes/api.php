c68aba8f067a62af17227b7752b05fc611ea27a4
erdemaltu <e-altug@hotmail.com>
2025-01-28 22:41:54 +0300

    Rate limit mechanism created

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LocationController;
 
-// Location API Routes
-Route::get('/locations', [LocationController::class, 'index']);
-Route::post('/locations', [LocationController::class, 'store']);
-Route::get('/locations/{id}', [LocationController::class, 'show']);
-Route::put('/locations/{id}', [LocationController::class, 'update']);
-Route::delete('/locations/{id}', [LocationController::class, 'destroy']);
-Route::post('/locations/optimized-route', [LocationController::class, 'optimizedRoute']);
+Route::middleware(['throttle:api'])->group(function () {
+    // Location API Routes
+    Route::get('/locations', [LocationController::class, 'index']);
+    Route::post('/locations', [LocationController::class, 'store']);
+    Route::get('/locations/{id}', [LocationController::class, 'show']);
+    Route::put('/locations/{id}', [LocationController::class, 'update']);
+    Route::delete('/locations/{id}', [LocationController::class, 'destroy']);
+    Route::post('/locations/optimized-route', [LocationController::class, 'optimizedRoute']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
